4d02faaa50a0db9ebe1fe2d37f6b9a2a8d8def53
brayan-alexis <brayanzxd@gmail.com>
2023-06-16 21:57:01 -0700

    Add Audio from mic function

--- speech-to-text/Program.cs
+++ speech-to-text/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
 Console.WriteLine("Hello, World!");
+
+var speechConfig = SpeechConfig.FromSubscription("", "centralus");
+
+async static Task AudioFromic(SpeechConfig speechConfig)
+{
+    using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+    using var recognizer = new SpeechRecognizer(speechConfig, "es-MX", audioConfig);
+    
+    Console.WriteLine("Speak into your microphone.");
+    var result = await recognizer.RecognizeOnceAsync();
+    Console.WriteLine(result.Text);
+}
