1a507197544c4466f3da5a53b960a01dffbf02c5
brayan-alexis <brayanzxd@gmail.com>
2023-06-17 23:50:31 -0700

    Implement Speech Recognition with Microphone and Audio File Options

--- speech-to-text/Program.cs
+++ speech-to-text/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome, again!");
 
-var speechConfig = SpeechConfig.FromSubscription("", "centralus");
+var speechConfig = SpeechConfig.FromSubscription("", ""); // Replace with your own subscription key and region
+// await AudioFromic(speechConfig); // Uncomment to use microphone
+// await AudioFromFile(speechConfig); // Uncomment to use audio file
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
 
 async static Task AudioFromic(SpeechConfig speechConfig)
 {
@@ -12,5 +17,15 @@ async static Task AudioFromic(SpeechConfig speechConfig)
     
     Console.WriteLine("Speak into your microphone.");
     var result = await recognizer.RecognizeOnceAsync();
-    Console.WriteLine(result.Text);
+    Console.WriteLine("You say: " + result.Text);
+}
+
+async static Task AudioFromFile(SpeechConfig speechConfig)
+{
+    using var audioConfig = AudioConfig.FromWavFileInput("audio.wav");
+    using var recognizer = new SpeechRecognizer(speechConfig, "es-MX", audioConfig);
+    
+    Console.WriteLine("Recognizing first result...");
+    var result = await recognizer.RecognizeOnceAsync();
+    Console.WriteLine("The audio says: " + result.Text);
 }
